5192c680dc5d6acd85e20b7ba9952e38fbd4b667
Brande Gilbert <brandegilbert@gmail.com>
2021-10-18 15:44:49 -0400

    Review after Redux review

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,47 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
-import { VideoContainer } from '../styled-components/VideoStyles'
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { VideoContainer } from "../styled-components/VideoStyles";
 
 export default function Videos() {
-    const URL = "https://www.googleapis.com/youtube/v3/videos"
-    const apiKey = `${process.env.REACT_APP_YOUTUBE_API_KEY}`
-    const dispatch = useDispatch()
+  const URL = "https://www.googleapis.com/youtube/v3/videos";
+  const apiKey = `${process.env.REACT_APP_YOUTUBE_API_KEY}`;
+  const dispatch = useDispatch();
 
-    useEffect (() => {
-        const getData = async() => {
-            const getTheVideos = await fetch(URL + "?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=12&regionCode=US&key="+apiKey,{
-                method: 'GET', 
-                // mode: 'cors', 
-                cache: 'no-cache', 
-                credentials: 'same-origin',
-                headers: {
-                'Content-Type': 'application/json'
-            },
-            });
-            const jsonData = await getTheVideos.json();
-            console.log(jsonData);
-            dispatch({
-                type: "GET_VIDEOS",
-                payload: {...jsonData.items}
-        });
-        };
-        getData();
-        return () => {};
-    }, [] );
-    
-    const titles = useSelector((state) => state.videos[0].snippet.channelTitle)
-    // const channelTitle = useSelector((state) => state[0].snippet.channelTitle)
-    // const views = useSelector((state) => state[0].statistics.viewCount)
-    // const time = useSelector((state) => state[0].snippet.publishedAt)
-    console.log(titles)
-    return (
-        <VideoContainer>
-            {titles}
-            
-            
-            
-        </VideoContainer>
-    )
+  useEffect(() => {
+    const getData = async () => {
+      const getTheVideos = await fetch(
+        URL +
+          "?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=12&regionCode=US&key=" +
+          apiKey,
+        {
+          method: "GET",
+          // mode: 'cors',
+          cache: "no-cache",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const jsonData = await getTheVideos.json();
+      console.log(jsonData);
+      dispatch({
+        type: "GET_VIDEOS",
+        payload: [...jsonData],
+      });
+    };
+    getData();
+    return () => {};
+  }, []);
+
+  const videos = useSelector((state) => state.items);
+
+  return (
+    <VideoContainer>
+      {videos.map((video) => (
+        <h3>{video.snippet.title}</h3>
+      ))}
+    </VideoContainer>
+  );
 }
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,13 +1,11 @@
-const initialState = {
-    videos:[],
-}
+const initialState = [];
 
 export const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "GET_VIDEOS":
-        return {...state, videos: action.payload};
+  switch (action.type) {
+    case "GET_VIDEOS":
+      return { ...action.payload };
 
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
