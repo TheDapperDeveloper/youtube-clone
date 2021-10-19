f86a7c50b0706b685e14b21ea2668db2430b9529
Brande Gilbert <brandegilbert@gmail.com>
2021-10-19 12:49:34 -0400

    Update to styling

--- src/styled-components/VideoStyles.js
+++ src/styled-components/VideoStyles.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
 export const VideoContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat (4, 3fr);
-  grid-template-rows: repeat(3, 3fr);
-gap: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  color: white;
+`;
 
-
-  `;
+// export const VideoContainer = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat (4, 3fr);
+//   grid-template-rows: repeat(3, 3fr);
+//   gap: 10px;
+// `;
